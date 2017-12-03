c8c40850d64ca0d5d4da1cc91f056e76e04eb618
Thomics <contot@gmail.com>
2017-12-03 02:39:36 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const express = require('express');
+const parser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+var port = process.env.PORT || 3000;
+
+//Sets the default path the be the public folder.
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+  request('http://api.pugetsound.onebusaway.org/api/where/arrivals-and-departures-for-stop/1_7040.json?key=TEST&minutesAfter=20', function (error, response, body) {
+    var json = JSON.parse(body);
+    return res.json(json);
+  });
+});
+
+server.listen(port, function(){
+  console.log('Server on port:' + port);
+});
